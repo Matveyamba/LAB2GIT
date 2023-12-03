c7327453781d2e2b8420f8b382d38848d3102aae
Matveyamba <matya2303@mail.ru>
2023-12-03 23:33:06 +0300

    Added divide function to function.cpp

--- function.cpp
+++ function.cpp
@@ -4,4 +4,12 @@
 double Function::multiply(double a, double b) {
     return a * b;
 }
+
+double Function::divide(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        // Обработка деления на ноль
+        return 0.0; // Можно заменить на другое значение или бросить исключение
+    }
 }
