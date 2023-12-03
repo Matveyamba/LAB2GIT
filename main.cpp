84fcc86e9fa3fecdfb1cbe4bb222367fda2f1763
Matveyamba <matya2303@mail.ru>
2023-12-03 23:04:00 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// main.cpp
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+int main() {
+    double a, b;
+    char operation;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+    }
+
+    return 0;
+}
